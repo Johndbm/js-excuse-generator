80e7654d819ce36e79bfc166b85170df322f12cc
john.banks <johndbanksm@gmail.com>
2022-11-14 17:44:00 +0000

    added some sample excuse variables arrays

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  //Variables Array
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  //Function
+
   console.log("Hello Rigo from the console!");
+  return;
 };
